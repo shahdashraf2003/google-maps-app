8bc399b74e3dfbe89969ed45299f812d8ce23b4c
shahdashraf2003 <shahdashraf396@gmail.com>
2025-02-10 21:49:14 +0200

    create place model

---
+++ lib/models/place_model.dart
@@ -0,0 +1,14 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class PlaceModel {
+  final int id;
+  final String name;
+  final LatLng latlng;
+  PlaceModel({required this.id, required this.name, required this.latlng});
+  
+  List<PlaceModel> places = [
+    PlaceModel(id: 1, name: 'صيدلية علام', latlng:LatLng(30.86317129083947, 32.309780567860194)),
+    PlaceModel(id: 1, name: 'كوبري السلام', latlng:LatLng(30.828696694093747, 32.317496371599184)),
+    PlaceModel(id: 1, name:'كنيسة ماري جرجس', latlng:LatLng(30.867892798813177, 32.325654874552164)),
+  ];
+}
